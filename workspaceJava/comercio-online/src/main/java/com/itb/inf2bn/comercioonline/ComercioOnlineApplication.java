15c7d1cb861dcfc8b49a461ecab15f3892f9856e
Kauã Ferreira Martins <rm88623@estudante.fieb.edu.br>
2023-08-09 22:43:37 -0300

    subindo projeto Java da Fieb

---
+++ workspaceJava/comercio-online/src/main/java/com/itb/inf2bn/comercioonline/ComercioOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2bn.comercioonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioOnlineApplication.class, args);
+	}
+
+}
---
+++ workspaceJava/comercio-online/src/test/java/com/itb/inf2bn/comercioonline/ComercioOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2bn.comercioonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
